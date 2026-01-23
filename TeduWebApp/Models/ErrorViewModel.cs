4b70803cc5eb237a0380a6985583e4fc34b024a1
Trung VH <vhtrung52@gmail.com>
2026-01-23 17:29:05 +0700

    init

---
+++ TeduWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TeduWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
